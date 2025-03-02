b223095cd85c663b061fd9b71a1ab216256c1c38
Ehab Hussein <ehabsmh3@gmail.com>
2025-03-03 01:38:30 +0200

    fix logout bug that preventing the user from logging out

--- src/ui/Navbar.tsx
+++ src/ui/Navbar.tsx
@@ -2,12 +2,13 @@ import { Link } from "react-router-dom";
 import { BiCart } from "react-icons/bi";
 import useAuth from "../hooks/useAuth";
 import { GrLogout, GrUserSettings } from "react-icons/gr";
+import { logout } from "../services/apiAuth";
 
 function Navbar() {
-  const { isAuthenticated } = useAuth();
+  const { isAuthenticated, handleCurrentUser } = useAuth();
 
   return (
-    <div className="flex max-h-[10vh] w-full items-center justify-between bg-white p-3">
+    <div className="flex w-full items-center justify-between bg-white p-3 lg:max-h-[10vh]">
       <div>
         <Link to="/" className="text-secondary text-2xl font-bold">
           Foods
@@ -22,39 +23,34 @@ function Navbar() {
       )}
 
       {isAuthenticated && (
-        <div className="flex items-center gap-14">
+        <div className="flex items-center gap-5 lg:gap-14">
           <div className="group flex cursor-pointer flex-col items-center justify-center text-sm">
             <Link
               to="/cart"
-              className="group-hover:text-gradient-2 text-gradient-1 duration-300"
+              className="group-hover:text-gradient-2 text-gradient-1 text-[0.9rem] duration-300"
             >
-              <BiCart
-                size={20}
-                className="text-gradient-1 group-hover:text-gradient-2 duration-300"
-              />
+              <BiCart className="text-gradient-1 group-hover:text-gradient-2 text-[0.9rem] duration-300 lg:text-[1.3rem]" />
               Cart
             </Link>
           </div>
 
           <Link
             to="/settings"
-            className="hover:text-gradient-2 group flex cursor-pointer flex-col items-center justify-center text-sm duration-300"
+            className="hover:text-gradient-2 group flex cursor-pointer flex-col items-center justify-center text-[0.9rem] text-sm duration-300"
           >
-            <GrUserSettings
-              size={20}
-              className="group-hover:text-gradient-2 duration-300"
-            />
+            <GrUserSettings className="group-hover:text-gradient-2 text-[0.9rem] duration-300 lg:text-[1.3rem]" />
             Settings
           </Link>
 
           <Link
-            to="/login"
-            className="hover:text-gradient-2 group flex cursor-pointer flex-col items-center justify-center text-sm duration-300"
+            to="/"
+            onClick={async () => {
+              await logout();
+              handleCurrentUser(null);
+            }}
+            className="hover:text-gradient-2 group flex cursor-pointer flex-col items-center justify-center text-[0.9rem] text-sm duration-300"
           >
-            <GrLogout
-              size={20}
-              className="group-hover:text-gradient-2 duration-300"
-            />
+            <GrLogout className="group-hover:text-gradient-2 text-[0.9rem] duration-300 lg:text-[1.3rem]" />
             logout
           </Link>
         </div>
