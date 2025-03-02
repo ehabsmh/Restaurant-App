08f308b19624f3e46ab620f5367add750ba75b6f
Ehab Hussein <ehabsmh3@gmail.com>
2025-03-03 01:39:41 +0200

    commenting out redux slices for later use

--- src/features/cart/CartSlice.tsx
+++ src/features/cart/CartSlice.tsx
@@ -1,59 +1,59 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { RootState } from "../../store";
+// import { createSlice } from "@reduxjs/toolkit";
+// import { RootState } from "../../store";
 
-interface ICartItem {
-  id: number;
-  itemId: number;
-  cartId: number;
-  sizeId: number;
-  quantity: number;
-  price: number;
-  pricePerQuantity: number;
-}
+// interface ICartItem {
+//   id: number;
+//   itemId: number;
+//   cartId: number;
+//   sizeId: number;
+//   quantity: number;
+//   price: number;
+//   pricePerQuantity: number;
+// }
 
-interface ICart {
-  id: number | null;
-  userId: string;
-  totalPrice: number | null;
-  numItems: number | null;
-  items: ICartItem[];
-}
+// interface ICart {
+//   id: number | null;
+//   userId: string;
+//   totalPrice: number | null;
+//   numItems: number | null;
+//   items: ICartItem[];
+// }
 
-const initialState: ICart = {
-  id: null,
-  userId: "",
-  totalPrice: null,
-  numItems: null,
-  items: [],
-};
+// const initialState: ICart = {
+//   id: null,
+//   userId: "",
+//   totalPrice: null,
+//   numItems: null,
+//   items: [],
+// };
 
-export const cartSlice = createSlice({
-  name: "cart",
-  initialState,
-  reducers: {
-    incrementQuantity(state, action) {},
-    decrementQuantity(state, action) {},
-    clearCart(state, action) {},
-    addCart(state, action) {
-      state.userId = action.payload.userId;
-      state.id = action.payload.id;
-    },
-    addToCart(state, action) {
-      state.items.push(action.payload);
-    },
-    changeItemSize(state, action) {},
-  },
-});
+// export const cartSlice = createSlice({
+//   name: "cart",
+//   initialState,
+//   reducers: {
+//     incrementQuantity(state, action) {},
+//     decrementQuantity(state, action) {},
+//     clearCart(state, action) {},
+//     addCart(state, action) {
+//       state.userId = action.payload.userId;
+//       state.id = action.payload.id;
+//     },
+//     addToCart(state, action) {
+//       state.items.push(action.payload);
+//     },
+//     changeItemSize(state, action) {},
+//   },
+// });
 
-export const {
-  incrementQuantity,
-  decrementQuantity,
-  clearCart,
-  addCart,
-  addToCart,
-  changeItemSize,
-} = cartSlice.actions;
+// export const {
+//   incrementQuantity,
+//   decrementQuantity,
+//   clearCart,
+//   addCart,
+//   addToCart,
+//   changeItemSize,
+// } = cartSlice.actions;
 
-export default cartSlice.reducer;
+// export default cartSlice.reducer;
 
-export const getCart = (state: RootState) => state.cart;
+// export const getCart = (state: RootState) => state.cart;
--- src/store.ts
+++ src/store.ts
@@ -1,16 +1,16 @@
-import { configureStore } from '@reduxjs/toolkit'
-import cartReducer from './features/cart/CartSlice'
+// import { configureStore } from '@reduxjs/toolkit'
+// import cartReducer from './features/cart/CartSlice'
 
 
-const store = configureStore({
-  reducer: {
-    cart: cartReducer
-  },
-})
+// const store = configureStore({
+//   reducer: {
+//     cart: cartReducer
+//   },
+// })
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>
-// Inferred type: {cart: cartState}
-export type AppDispatch = typeof store.dispatch
+// // Infer the `RootState` and `AppDispatch` types from the store itself
+// export type RootState = ReturnType<typeof store.getState>
+// // Inferred type: {cart: cartState}
+// export type AppDispatch = typeof store.dispatch
 
-export default store;
+// export default store;
